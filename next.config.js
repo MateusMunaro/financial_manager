a03841d9f9c0f1cd14a2ab42ba907c1ed7cfaecb
gabrielchboff <gabrielchagasboff@gmail.com>
2025-10-23 18:00:50 -0300

    FEATURE: Linux native app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Disable server-side features since we're exporting a static site
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
