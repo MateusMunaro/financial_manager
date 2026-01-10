7675789aa961f357dddf01767d20e1e7187a1cf7
Mateus Augusto Munaro de Souza <mateusmunaro40@gmail.com>
2026-01-10 20:02:41 -0300

    teste

--- backend/app/schemas/dashboard.py
+++ backend/app/schemas/dashboard.py
@@ -2,7 +2,7 @@ from typing import List
 from pydantic import BaseModel
 from app.schemas.expense import Period
 
-
+#teste
 class FinancialChangePercentage(BaseModel):
     balance: float
     income: float
